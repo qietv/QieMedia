3db595a301f23aa9f738fcd7a7adbc90bba2f32c
MouriNaruto <Kenji.Mouri@outlook.com>
2025-02-18 18:57:49 +0800

    Add QieMedia library project.

---
+++ Sources/QieMedia/QieMedia.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   QieMedia
+ * FILE:      QieMedia.cpp
+ * PURPOSE:   Implementation for QieMedia library for OpenHarmony
+ *
+ * LICENSE:   See License.md file in the project root
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "QieMedia.h"
+
+extern "C" void QieMediaEmptySymbol()
+{
+    // Do Nothing
+}
---
+++ Sources/QieMedia/QieMedia.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   QieMedia
+ * FILE:      QieMedia.h
+ * PURPOSE:   Definition for QieMedia library for OpenHarmony
+ *
+ * LICENSE:   See License.md file in the project root
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef QIEMEDIA
+#define QIEMEDIA
+
+#include <stdint.h>
+
+#endif // !QIEMEDIA
