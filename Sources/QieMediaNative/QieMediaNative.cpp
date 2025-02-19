93f38b760bfb96037f35566d30b0d2f72481e5a8
MouriNaruto <Kenji.Mouri@outlook.com>
2025-02-19 18:11:11 +0800

    Rename QieMedia subproject to QieMediaNative.

---
+++ Sources/QieMediaNative/QieMediaNative.cpp
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   QieMedia
+ * FILE:      QieMediaNative.cpp
+ * PURPOSE:   Implementation for QieMedia native library for OpenHarmony
+ *
+ * LICENSE:   See License.md file in the project root
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#include "QieMediaNative.h"
+
+extern "C" void QieMediaNativeEmptySymbol()
+{
+    // Do Nothing
+}
---
+++ Sources/QieMediaNative/QieMediaNative.h
@@ -0,0 +1,16 @@
+﻿/*
+ * PROJECT:   QieMedia
+ * FILE:      QieMediaNative.h
+ * PURPOSE:   Definition for QieMedia native library for OpenHarmony
+ *
+ * LICENSE:   See License.md file in the project root
+ *
+ * MAINTAINER: MouriNaruto (Kenji.Mouri@outlook.com)
+ */
+
+#ifndef QIEMEDIA_NATIVE
+#define QIEMEDIA_NATIVE
+
+#include <stdint.h>
+
+#endif // !QIEMEDIA_NATIVE
