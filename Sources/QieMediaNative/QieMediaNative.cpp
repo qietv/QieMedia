57807a4cec0ac1a54b06512567cd4b7dabb92b09
MouriNaruto <Kenji.Mouri@outlook.com>
2025-02-20 19:00:10 +0800

    Define a draft of interop interfaces.

--- Sources/QieMediaNative/QieMediaNative.cpp
+++ Sources/QieMediaNative/QieMediaNative.cpp
@@ -14,3 +14,31 @@ extern "C" void QieMediaNativeEmptySymbol()
 {
     // Do Nothing
 }
+
+//
+// CreatePlaybackSession(std::string SessionId)
+// ClosePlaybackSession(std::string SessionId)
+// QueryPlaybackSession() // JSON string
+//
+// PlaybackSession
+//
+// SetFileSource(int FileDescriptor)
+// SetUriSource(std::string Uri)
+// SetSurface(int SurfaceId)
+//
+// Load()
+// Reset()
+// 
+// Pause()
+// Play()
+// Stop() is Pause() and Position = 0
+//
+// Position { get; set; }
+// PlaybackRate { get; set; }
+// Volume { get; set; }
+// PlaybackState { get; } enum MediaPlaybackState { None, Opening, Buffering, Playing, Paused, Stopped }
+// IsLooping { get; set; }
+// NaturalDuration { get; } is the actual duration of the media without applying the PlaybackRate
+// IsMuted { get; set; }
+// Information { get; } // JSON string
+//
